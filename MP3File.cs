4687f95e4800d4131e1b7d1f3eeb78b7b951d31c
Trontor <rjoshi1@93KS8W1.standrewscc.local>
2016-02-08 21:25:10 +1000

    Modulised
    Modulised

---
+++ MP3File.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MP3_File_Auto_Tagger
+{
+    internal class Mp3File
+    {
+        private List<string> Artists = new List<string>();
+
+        public string FilePath { get; set; }
+        public string FileName { get; set; }
+        public string BaseArtist { get; set; }
+
+        public string Start
+        {
+            get { return PartOfFileName(FileName, true); }
+        }
+
+        public string End
+        {
+            get { return PartOfFileName(FileName, false); }
+        }
+
+        public int SplitHeifenKey { get; set; }
+
+        private static IEnumerable<string> SplitBy(string inputString, char c, int splitIndex)
+        {
+            var test = new List<string>();
+            switch (splitIndex)
+            {
+                case 0:
+                    test.AddRange(inputString.Split(c));
+                    break;
+                case 1:
+                    string str1 = inputString.Split(c)[0];
+                    string str2 = inputString.Split(c)[1];
+                    string str3 = inputString.Split(c)[2];
+                    string end = str2 + c + str3;
+                    test.Add(str1);
+                    test.Add(end);
+                    break;
+            }
+            return test.ToArray();
+        }
+
+        public string FixFileName(string path)
+        {
+            if (path == null) throw new ArgumentNullException("path");
+            FilePath = path;
+            FileName = Path.GetFileName(path).Replace(".mp3", "");
+            FindAndReplace();
+            CompleteFeaturingBrackets();
+            ExtractArtists();
+            AttachFeaturedArtists();
+            WriteId3Tags();
+            return FileName;
+        }
+
+        private void WriteId3Tags()
+        {
+            var file = TagLib.File.Create(FilePath);
+            if (FileName.Contains("-"))
+            {
+                var split = SplitBy(FileName.Replace(".mp3", ""), '-', SplitHeifenKey).ToArray();
+                string title = split[1].Trim();
+                file.Tag.Performers = null;
+                file.Tag.Performers = Artists.ToArray<string>();
+                file.Tag.Title = title;
+                file.Save();
+            }
+            file.Dispose();
+        }
+
+        private void AttachFeaturedArtists()
+        {
+            if (Artists.Count > 1)
+                FileName += " (ft. ";
+            Artists.Reverse();
+            Artists = Artists.OrderBy(x => x != BaseArtist).ToList();
+            for (int i = 0; i < Artists.Count; i++)
+            {
+                Artists[i] = Artists[i].Trim();
+            }
+            string result = FileName;
+            List<string> orderedList = Artists.OrderBy(x => x).ToList();
+            foreach (string str in orderedList)
+            {
+                if (str != BaseArtist)
+                    result = result + str +
+                             ((orderedList.IndexOf(str) == Artists.Count - 1) || Artists.Count == 2 ? "" : ", ");
+            }
+            FileName = result;
+            if (Artists.Count > 1)
+                FileName = FileName.Trim() + ")";
+        }
+
+        private static int IndexOfNth(string str, char c, int n)
+        {
+            int s = -1;
+            for (int i = 0; i < n; i++)
+            {
+                s = str.IndexOf(c, s + 1);
+
+                if (s == -1) break;
+            }
+            if (s < -1)
+                return s;
+
+            while (str[s - 1] == ' ')
+            {
+                s -= 1;
+            }
+            return s == -1 ? IndexOfNth(str, c, n - 1) : s;
+        }
+
+        private string PartOfFileName(string s, bool first)
+        {
+            int instanceCount = s.Count(f => f == '-');
+            if (instanceCount > 1 && SplitHeifenKey == 0)
+            {
+                bool notReplaced = true;
+                while (notReplaced)
+                {
+                    notReplaced = false;
+                    Console.WriteLine("----------------------------");
+                    Console.WriteLine("The file:{0} has more than one heifen:", FileName);
+                    Console.WriteLine("Which heifen do you want to use?");
+                    Console.WriteLine("Write an integer value corresponding with its occurence count (1 - {0}).",
+                        instanceCount);
+                    Console.WriteLine("----------------------------");
+                    Console.WriteLine("I want to split heifen number: ");
+                    int key;
+                    if (!int.TryParse(Console.ReadLine(), out key)) continue;
+                    SplitHeifenKey = key;
+                    if (SplitHeifenKey > instanceCount)
+                    {
+                        Console.WriteLine("Sorry! That number exceeds the amount of heifens in the filename :^(");
+                        notReplaced = true;
+                    }
+                }
+            }
+
+            var filePaths = SplitBy(s.Trim(), '-', SplitHeifenKey);
+            string startOfFile = filePaths.ToArray()[Convert.ToInt32(!first)].Trim();
+            return startOfFile;
+        }
+
+        private void FindAndReplace()
+        {
+            var replaceList = new Dictionary<string, string>
+            {
+                {"OFFICIAL", ""},
+                {"FEATURING", "FT"},
+                {" featuring ", "ft.  "},
+                {" (Featuring ", " (ft. "},
+                {" FEAT", " (FEAT"},
+                {"(FEAT", "(feat"},
+                {"FEAT", "ft"},
+                {"FT ", "ft. "},
+                {"Ft ", "ft. "},
+                {"ft ", "ft. "},
+                {" FT. ", " (ft. "},
+                {" FT", "(ft"},
+                {"(FT ", "(ft. "},
+                {" (ft ", " (ft."},
+                {" (Ft ", "(ft. "}
+            };
+            while (true)
+            {
+                bool replaced = false;
+                foreach (
+                    var replaceItem in
+                        replaceList.Where(replaceItem => FileName.ToLower().Contains(replaceItem.Key.ToLower())))
+                {
+                    FileName = FileName.Replace(replaceItem.Key.ToLower(), replaceItem.Value);
+                    FileName = FileName.Replace(replaceItem.Key, replaceItem.Value);
+                    replaced = true;
+                }
+                if (replaced) continue;
+                break;
+            }
+        }
+
+        private void CompleteFeaturingBrackets()
+        {
+            if (Start.Contains("(ft."))
+            {
+                int insertIndex;
+                int openParenthesesCount = Start.Split('(').Length - 1;
+                if (openParenthesesCount > 1)
+                    insertIndex = IndexOfNth(Start, '(', 1);
+                else insertIndex = Start.Length;
+                if (Start[insertIndex - 1] != ')')
+                    FileName = Start.Insert(insertIndex, ")") + " - " + End;
+            }
+            if (End.Contains("(ft."))
+            {
+                int insertIndex;
+                int openParenthesesCount = End.Split('(').Length - 1;
+                if (openParenthesesCount > 1)
+                    insertIndex = IndexOfNth(End, '(', 2);
+                else insertIndex = End.Length;
+                if (End[insertIndex - 1] != ')')
+                    FileName = Start + " - " + End.Insert(insertIndex, ")");
+            }
+        }
+
+        private void ExtractArtists()
+        {
+            string newStart = null;
+            string newEnd = null;
+            string baseArtist;
+            if (Start.Contains("(ft."))
+            {
+                int ftIndex = Start.IndexOf("(ft.", StringComparison.Ordinal);
+                string featuringStart = Start.Substring(ftIndex);
+                string featuringFinal = featuringStart.Remove(featuringStart.IndexOf(")", StringComparison.Ordinal));
+                baseArtist = Start.Remove(ftIndex).Trim();
+                var artists = new List<string>();
+                if (featuringFinal.Contains(","))
+                {
+                    artists = featuringFinal.Replace("(ft.", "").Trim()
+                        .Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries)
+                        .ToList();
+                }
+                else
+                    artists.Add(featuringFinal.Replace("(ft.", "").Trim());
+                Artists.AddRange(artists);
+                newStart = Start.Replace(featuringStart, "").Trim();
+            }
+            else
+                baseArtist = Start;
+
+            Artists.Add(baseArtist);
+            if (End.Contains("(ft."))
+            {
+                int ftIndex = End.IndexOf("(ft.", StringComparison.Ordinal);
+                string featuringEnd = End.Substring(ftIndex);
+                string featuringFinal = featuringEnd.Remove(featuringEnd.IndexOf(")", StringComparison.Ordinal));
+                var artists = new List<string>();
+                if (featuringFinal.Contains(","))
+                {
+                    artists = featuringFinal.Replace("(ft.", "").Trim()
+                        .Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries)
+                        .ToList();
+                }
+                else
+                    artists.Add(featuringFinal.Replace("(ft.", "").Trim());
+                Artists.AddRange(artists);
+                newEnd = End.Replace(featuringEnd, "").Trim();
+            }
+
+            Artists.Reverse();
+            if (newStart == null)
+                newStart = Start;
+            if (newEnd == null)
+                newEnd = End;
+            BaseArtist = baseArtist;
+            FileName = newStart + " - " + newEnd;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,36 +7,36 @@ using File = TagLib.File;
 
 namespace MP3_File_Auto_Tagger
 {
-    public static class StringExtensions
-    {
-        public static int Separate(this string str, char value, int nth = 1)
-        {
-            if (nth <= 0)
-                throw new ArgumentException("Can not find the zeroth index of substring in string. Must start with 1");
-            int offset = str.IndexOf(value.ToString(), StringComparison.Ordinal);
-            for (int i = 1; i < nth; i++)
-            {
-                if (offset == -1) return -1;
-                offset = str.IndexOf(value.ToString(), offset + 1, StringComparison.Ordinal);
-            }
-            return offset;
-        }
+    //public static class StringExtensions
+    //{
+    //    public static int Separate(this string str, char value, int nth = 1)
+    //    {
+    //        if (nth <= 0)
+    //            throw new ArgumentException("Can not find the zeroth index of substring in string. Must start with 1");
+    //        int offset = str.IndexOf(value.ToString(), StringComparison.Ordinal);
+    //        for (int i = 1; i < nth; i++)
+    //        {
+    //            if (offset == -1) return -1;
+    //            offset = str.IndexOf(value.ToString(), offset + 1, StringComparison.Ordinal);
+    //        }
+    //        return offset;
+    //    }
 
-        public static string StartOfFile(this string s)
-        {
-            var filePaths = s?.Trim().Split(new[] { "-" }, StringSplitOptions.None);
-            string startOfFile = filePaths?[0].Trim();
-            return startOfFile;
-        }
+    //    public static string StartOfFile(this string s)
+    //    {
+    //        var filePaths = s?.Trim().Split(new[] { "-" }, StringSplitOptions.None);
+    //        string startOfFile = filePaths?[0].Trim();
+    //        return startOfFile;
+    //    }
 
-        public static string EndOfFile(this string s)
-        {
-            //Returns: string[0] = Artist FEAT testIfy  string[1] = Title ft test
-            var filePaths = s?.Trim().Split(new[] { "-" }, StringSplitOptions.None);
-            string endOfFile = filePaths?[1].Trim();
-            return endOfFile;
-        }
-    }
+    //    public static string EndOfFile(this string s)
+    //    {
+    //        //Returns: string[0] = Artist FEAT testIfy  string[1] = Title ft test
+    //        var filePaths = s?.Trim().Split(new[] { "-" }, StringSplitOptions.None);
+    //        string endOfFile = filePaths?[1].Trim();
+    //        return endOfFile;
+    //    }
+    //}
 
     internal class Program
     {
@@ -45,7 +45,7 @@ namespace MP3_File_Auto_Tagger
 
         private static void Main(string[] args)
         {
-            var path = @"D:\Music - Copy";
+            var path = @"D:\Music";
             var monitor = new FileSystemWatcher(path, "*.mp3") { EnableRaisingEvents = true };
             monitor.Created += monitor_CreatedOrChanged;
             monitor.Renamed += monitor_CreatedOrChanged;
@@ -53,130 +53,132 @@ namespace MP3_File_Auto_Tagger
             //  ShowWindow(handle, SW_HIDE);
 
             var files = Directory.GetFiles(path, "*.mp3", SearchOption.TopDirectoryOnly);
-            foreach (string fileName in files)
+            foreach (string filePath in files)
             {
-                if (!fileName.Contains(".ini"))
+                if (!filePath.Contains(".ini"))
                 {
-                    Console.WriteLine(FixFilename(fileName));
+                    Mp3File file = new Mp3File();
+                    string finalName = file.FixFileName(filePath);
+                    Console.WriteLine(finalName);
                 }
             }
             Console.ReadKey();
         }
 
-        /// <summary>
-        ///     Make sure you are passing a PATH not a FILENAME
-        /// </summary>
-        /// <param name="path"></param>
-        /// <returns></returns>
-        /// Path Format : D:\Music\Artist FEAT testIfy - Title ft test.mp3
-        private static string FixFilename(string path)
-        {
-            var replaceList = new Dictionary<string, string>
-            {
-                {"OFFICIAL", ""},
-                {"FEATURING", "FT"},
-                {"FEAT", "ft"},
-                {"FT ", "ft. "},
-                {"Ft ", "ft. "},
-                {" FEAT", " (FEAT"},
-                {"(FEAT", "(feat"},
-                {" FT. ", " (ft. "},
-                {" FT", "(ft"},
-                {"(FT ", "(ft. "}
-            };
+        ///// <summary>
+        /////     Make sure you are passing a PATH not a FILENAME
+        ///// </summary>
+        ///// <param name="path"></param>
+        ///// <returns></returns>
+        ///// Path Format : D:\Music\Artist FEAT testIfy - Title ft test.mp3
+        //private static string FixFilename(string path)
+        //{
+        //    var replaceList = new Dictionary<string, string>
+        //    {
+        //        {"OFFICIAL", ""},
+        //        {"FEATURING", "FT"},
+        //        {"FEAT", "ft"},
+        //        {"FT ", "ft. "},
+        //        {"Ft ", "ft. "},
+        //        {" FEAT", " (FEAT"},
+        //        {"(FEAT", "(feat"},
+        //        {" FT. ", " (ft. "},
+        //        {" FT", "(ft"},
+        //        {"(FT ", "(ft. "}
+        //    };
 
-            //Returns Artist FEAT testIfy - Title ft test.mp3
-            string fileName = Path.GetFileName(path);
+        //    //Returns Artist FEAT testIfy - Title ft test.mp3
+        //    string fileName = Path.GetFileName(path);
 
-            //Returns Artist FEAT testIfy - Title ft test 
-            string fileNameWithoutExtension = fileName?.Replace(".mp3", "");
-            string newFileName = fileNameWithoutExtension;
-            if (newFileName == null) throw new ArgumentNullException(nameof(newFileName));
+        //    //Returns Artist FEAT testIfy - Title ft test 
+        //    string fileNameWithoutExtension = fileName?.Replace(".mp3", "");
+        //    string newFileName = fileNameWithoutExtension;
+        //    if (newFileName == null) throw new ArgumentNullException(nameof(newFileName));
 
 
-            foreach (var replaceItem in replaceList)
-            {
-                newFileName = newFileName.Replace(replaceItem.Key.ToLower(), replaceItem.Value);
-                newFileName = newFileName.Replace(replaceItem.Key, replaceItem.Value);
-            }
+        //    foreach (var replaceItem in replaceList)
+        //    {
+        //        newFileName = newFileName.Replace(replaceItem.Key.ToLower(), replaceItem.Value);
+        //        newFileName = newFileName.Replace(replaceItem.Key, replaceItem.Value);
+        //    }
 
-            if (newFileName.StartOfFile().Contains("(ft.") &&
-                newFileName.StartOfFile()[newFileName.StartOfFile().Split('(').Count() > 1 ? newFileName.StartOfFile().Separate('(', 2) : newFileName.EndOfFile().Length - 1] != ')')
-                newFileName = newFileName.StartOfFile().Insert(newFileName.StartOfFile().Separate('(', 2), ")") + " - " +
-                              newFileName.EndOfFile();
+        //    if (newFileName.StartOfFile().Contains("(ft.") &&
+        //        newFileName.StartOfFile()[newFileName.StartOfFile().Split('(').Count() > 1 ? newFileName.StartOfFile().Separate('(', 2) : newFileName.EndOfFile().Length - 1] != ')')
+        //        newFileName = newFileName.StartOfFile().Insert(newFileName.StartOfFile().Separate('(', 2), ")") + " - " +
+        //                      newFileName.EndOfFile();
 
-            if (newFileName.EndOfFile().Contains("(ft.") &&
-                newFileName.EndOfFile()[newFileName.EndOfFile().Split('(').Count() > 1 ? newFileName.EndOfFile().Separate('(', 2) : newFileName.EndOfFile().Length - 1] != ')')
-                newFileName = newFileName.StartOfFile() + " - " +
-                              newFileName.EndOfFile().Insert(newFileName.EndOfFile().Separate('(', 2), ")");
+        //    if (newFileName.EndOfFile().Contains("(ft.") &&
+        //        newFileName.EndOfFile()[newFileName.EndOfFile().Split('(').Count() > 1 ? newFileName.EndOfFile().Separate('(', 2) : newFileName.EndOfFile().Length - 1] != ')')
+        //        newFileName = newFileName.StartOfFile() + " - " +
+        //                      newFileName.EndOfFile().Insert(newFileName.EndOfFile().Separate('(', 2), ")");
 
-            string start = null;
-            string end = null;
-            string baseArtist;
-            var artists = new List<string>();
-            if (newFileName.StartOfFile().Contains("(ft."))
-            {
-                int ftIndex = newFileName.StartOfFile().IndexOf("(ft.", StringComparison.Ordinal);
-                string subString = newFileName.StartOfFile().Substring(ftIndex);
-                string subStringEnd = subString.Remove(subString.IndexOf(")", StringComparison.Ordinal));
-                baseArtist = newFileName.StartOfFile().Remove(ftIndex).Trim();
-                artists.AddRange(subStringEnd.Replace("(ft.", "").Split(new[] { "," }, StringSplitOptions.None).ToList());
-                start = newFileName.StartOfFile().Replace(subString, "").Trim();
-            }
-            else
-                baseArtist = newFileName.StartOfFile();
+        //    string start = null;
+        //    string end = null;
+        //    string baseArtist;
+        //    var artists = new List<string>();
+        //    if (newFileName.StartOfFile().Contains("(ft."))
+        //    {
+        //        int ftIndex = newFileName.StartOfFile().IndexOf("(ft.", StringComparison.Ordinal);
+        //        string subString = newFileName.StartOfFile().Substring(ftIndex);
+        //        string subStringEnd = subString.Remove(subString.IndexOf(")", StringComparison.Ordinal));
+        //        baseArtist = newFileName.StartOfFile().Remove(ftIndex).Trim();
+        //        artists.AddRange(subStringEnd.Replace("(ft.", "").Split(new[] { "," }, StringSplitOptions.None).ToList());
+        //        start = newFileName.StartOfFile().Replace(subString, "").Trim();
+        //    }
+        //    else
+        //        baseArtist = newFileName.StartOfFile();
 
-            artists.Add(baseArtist);
-            if (newFileName.EndOfFile().Contains("(ft."))
-            {
-                int ftIndex = newFileName.EndOfFile().IndexOf("(ft", StringComparison.Ordinal);
-                string subString = newFileName.EndOfFile().Substring(ftIndex);
-                string subStringEnd = subString.Remove(subString.IndexOf(")", StringComparison.Ordinal));
+        //    artists.Add(baseArtist);
+        //    if (newFileName.EndOfFile().Contains("(ft."))
+        //    {
+        //        int ftIndex = newFileName.EndOfFile().IndexOf("(ft", StringComparison.Ordinal);
+        //        string subString = newFileName.EndOfFile().Substring(ftIndex);
+        //        string subStringEnd = subString.Remove(subString.IndexOf(")", StringComparison.Ordinal));
 
-                artists.AddRange(subStringEnd.Replace("(ft.", "").Split(new[] { "," }, StringSplitOptions.None).ToList());
+        //        artists.AddRange(subStringEnd.Replace("(ft.", "").Split(new[] { "," }, StringSplitOptions.None).ToList());
 
-                end = newFileName.EndOfFile().Replace(subString, "").Trim();
-            }
-            artists.Reverse();
+        //        end = newFileName.EndOfFile().Replace(subString, "").Trim();
+        //    }
+        //    artists.Reverse();
 
-            if (start == null)
-                start = newFileName.StartOfFile();
-            if (end == null)
-                end = newFileName.EndOfFile();
+        //    if (start == null)
+        //        start = newFileName.StartOfFile();
+        //    if (end == null)
+        //        end = newFileName.EndOfFile();
 
-            string finalFileName = start + " - " + end;
+        //    string finalFileName = start + " - " + end;
 
-            if (artists.Count > 1)
-                finalFileName += " (ft. ";
-            artists.Reverse();
-            artists = artists.OrderBy(x => x != baseArtist).ToList();
-            for (var i = 0; i < artists.Count; i++)
-            {
-                artists[i] = artists[i].Trim();
-            }
-            finalFileName = artists.Where(str => str != baseArtist)
-                .Aggregate(finalFileName,
-                    (current, str) =>
-                        current + str +
-                        ((artists.IndexOf(str) == artists.Count - 1) || artists.Count == 2 ? "" : ", "));
-            if (artists.Count > 1)
-                finalFileName = finalFileName.Trim() + ")";
+        //    if (artists.Count > 1)
+        //        finalFileName += " (ft. ";
+        //    artists.Reverse();
+        //    artists = artists.OrderBy(x => x != baseArtist).ToList();
+        //    for (var i = 0; i < artists.Count; i++)
+        //    {
+        //        artists[i] = artists[i].Trim();
+        //    }
+        //    finalFileName = artists.Where(str => str != baseArtist)
+        //        .Aggregate(finalFileName,
+        //            (current, str) =>
+        //                current + str +
+        //                ((artists.IndexOf(str) == artists.Count - 1) || artists.Count == 2 ? "" : ", "));
+        //    if (artists.Count > 1)
+        //        finalFileName = finalFileName.Trim() + ")";
 
-            var file = File.Create(path);
-            if (newFileName.Contains("-"))
-            {
-                var split = finalFileName.Replace(".mp3", "").Split('-');
-                string title = split[1].Trim();
-                string artist = split[0].Trim();
-                file.Tag.Performers = null;
-                file.Tag.Performers = artists.ToArray<string>();
-                file.Tag.Title = title;
-                file.Save();
-            }
-            file.Dispose();
+        //    var file = File.Create(path);
+        //    if (newFileName.Contains("-"))
+        //    {
+        //        var split = finalFileName.Replace(".mp3", "").Split('-');
+        //        string title = split[1].Trim();
+        //        string artist = split[0].Trim();
+        //        file.Tag.Performers = null;
+        //        file.Tag.Performers = artists.ToArray<string>();
+        //        file.Tag.Title = title;
+        //        file.Save();
+        //    }
+        //    file.Dispose();
 
-            return finalFileName;
-        }
+        //    return finalFileName;
+        //}
 
         [DllImport("kernel32.dll")]
         private static extern IntPtr GetConsoleWindow();
